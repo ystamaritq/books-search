e0bb9437b00db817fd04ac437abcb69b302bc32f
Yadira Tamarit <ystamaritq@gmail.com>
2020-09-12 16:46:02 -0500

    Update  nav icons

--- client/src/components/SiteHeader/SiteHeader.js
+++ client/src/components/SiteHeader/SiteHeader.js
@@ -9,8 +9,8 @@ import {
 	SkipToContent,
 } from "carbon-components-react/lib/components/UIShell";
 
-import Notification20 from "@carbon/icons-react/lib/notification/20";
-import UserAvatar20 from "@carbon/icons-react/lib/user--avatar/20";
+import Search20 from "@carbon/icons-react/lib/search/20";
+import Bookmark20 from "@carbon/icons-react/lib/bookmark/20";
 import AppSwitcher20 from "@carbon/icons-react/lib/app-switcher/20";
 
 import { Link } from "react-router-dom";
@@ -30,11 +30,11 @@ const SiteHeader = () => (
 			</HeaderMenuItem>
 		</HeaderNavigation>
 		<HeaderGlobalBar>
-			<HeaderGlobalAction aria-label="Notifications">
-				<Notification20 />
+			<HeaderGlobalAction aria-label="Search">
+				<Search20 />
 			</HeaderGlobalAction>
 			<HeaderGlobalAction aria-label="User Avatar">
-				<UserAvatar20 />
+				<Bookmark20 />
 			</HeaderGlobalAction>
 			<HeaderGlobalAction aria-label="App Switcher">
 				<AppSwitcher20 />
