e0d03eae38ff1434731f0423cc014619c1a60f45
Yadira Tamarit <ystamaritq@gmail.com>
2020-09-13 14:41:24 -0500

    Adds additional properties to book entity

--- models/book.js
+++ models/book.js
@@ -3,9 +3,10 @@ const Schema = mongoose.Schema;
 
 const bookSchema = new Schema({
 	title: { type: String, required: true },
-	authors: { type: String, required: true },
+	subtitle: { type: String, required: true },
+	authors: { type: Array, required: true },
 	description: { type: String, required: true },
-	image: { type: String, required: true },
+	thumbnail: { type: String, required: true },
 	link: { type: String, required: true },
 });
 
