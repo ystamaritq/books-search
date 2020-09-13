6bdc1b875f297a21952501dc1745e51cc45f55d5
Yadira Tamarit <ystamaritq@gmail.com>
2020-09-13 10:41:57 -0500

    added book search card

--- client/src/pages/SearchPage/SearchPage.js
+++ client/src/pages/SearchPage/SearchPage.js
@@ -1,6 +1,7 @@
 import React from "react";
 import RowCard from "./../../components/RowCard";
 import BookSearchForm from "./../../components/BookSearchForm";
+import BookCardSearch from "../../components/BookCardSearch";
 
 const SearchPage = () => {
 	const onSearch = (values) => {
@@ -15,6 +16,11 @@ const SearchPage = () => {
 			</RowCard>
 			<RowCard>
 				<h2>Results</h2>
+				<BookCardSearch
+					cover="https://via.placeholder.com/150"
+					title="Book Search Title Fetch"
+					description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+				/>
 			</RowCard>
 		</>
 	);
