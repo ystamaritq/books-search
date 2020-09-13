ef494ac0c2dfbfbd4242af7c851ae88d01f3f5dc
Yadira Tamarit <ystamaritq@gmail.com>
2020-09-13 14:44:03 -0500

    Implements book search UI integration with API

--- client/src/pages/SearchPage/SearchPage.js
+++ client/src/pages/SearchPage/SearchPage.js
@@ -1,11 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 import RowCard from "./../../components/RowCard";
 import BookSearchForm from "./../../components/BookSearchForm";
-import BookCardSearch from "../../components/BookCardSearch";
+import BookCard from "../../components/BookCard";
+import axios from "axios";
+import { notification } from "antd";
 
 const SearchPage = () => {
-	const onSearch = (values) => {
+	const [books, setBooks] = useState([]);
+
+	const onSave = ({
+		thumbnail,
+		title,
+		subtitle,
+		description,
+		authors,
+		link,
+	}) => {
+		// FIXME
+		notification.success({
+			message: `Successfully saved ${title}`,
+			description: "This will now show in your saved books",
+		});
+	};
+
+	const onSearch = async (values) => {
 		console.log("Received values of form: ", values);
+
+		var config = {
+			method: "get",
+			url: `/api/books/search?search=${values.book}`,
+			headers: {},
+		};
+
+		axios(config)
+			.then(function (response) {
+				console.log(response.data);
+				setBooks(response.data);
+			})
+			.catch(function (error) {
+				console.log(error);
+			});
 	};
 
 	return (
@@ -16,11 +50,18 @@ const SearchPage = () => {
 			</RowCard>
 			<RowCard>
 				<h2>Results</h2>
-				<BookCardSearch
-					cover="https://via.placeholder.com/150"
-					title="Book Search Title Fetch"
-					description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-				/>
+				{books.map((book, index) => (
+					<BookCard
+						key={index}
+						thumbnail={book.thumbnail}
+						title={book.title}
+						subtitle={book.subtitle}
+						description={book.description}
+						authors={book.authors}
+						link={book.link}
+						action={onSave}
+					/>
+				))}
 			</RowCard>
 		</>
 	);
